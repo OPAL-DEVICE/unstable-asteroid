425040dd7f1b5f6a38830025b2e28eecc777a6b1
Caroline Wong <carowong5@gmail.com>
2014-12-09 16:39:15 -0800

    changed user and lobby models, added addNewLobby and addNewUser functionality in controllers

--- server/lobbies/lobbyController.js
+++ server/lobbies/lobbyController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Message  = require('./messageModel'),
+var Message  = require('./lobbyModel'),
     Q        = require('q'),
     mongoose = require('mongoose');
 
@@ -16,9 +16,26 @@ mongoose.connect('mongodb://MongoLab-d:tsWFfWiQkrxfZhKZbNOBPVGp3culnVTNs5G7nyd1c
   * create and store lobby object
   * @params [Function] callback to be called after successful retrieval
   */
-  addNewLobby: function(lobbyObject, callback) {
+  addNewLobby: function(lobbyObject, userObject, callback) { 
+   var newLobby = {
+      name: lobbyObject.name,
+      usersId: [userObject.userId],
+      messages: [],
+      password: '',
+      createdBy: [mongoose.Schema.Types.ObjectId]
+   };
     
-  }
+    //creates promises of query functions
+    var createLobby = Q.nbind(Lobby.create, Lobby);
+
+    //creates and saves the lobby to the DB
+    createLobby(newLobby)
+    .then(function(createdLobby) {
+      return createdLobby;
+    }); 
+
+  } //,
+
 
   /**
   * enter lobby when given lobby name
@@ -34,6 +51,12 @@ mongoose.connect('mongodb://MongoLab-d:tsWFfWiQkrxfZhKZbNOBPVGp3culnVTNs5G7nyd1c
   * create and store lobby object. if password is null, public lobby
   * @params [Function] callback to be called after successful retrieval
   */
+
+  /**
+  * adds people to lobby when join
+  * @params [Function] callback to be called after successful retrieval
+  */
+
 
   
 
--- server/users/userModel.js
+++ server/users/userModel.js
@@ -5,7 +5,8 @@ var mongoose = require('mongoose');
 //creates schema for messages with properties message, parentID, and childrenID
 //unqiue _id created by default
 var UserSchema = new mongoose.Schema({
-  name: String
+  name: String,
+  password: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
