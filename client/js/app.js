96633d8be6080c78ad3b9ab55ae08634217446f6
Phung.Doug@gmail.com <phung.doug@gmail.com>
2014-12-12 17:36:18 -0800

    menu changes

--- client/js/app.js
+++ client/js/app.js
@@ -160,4 +160,25 @@ $(document).ready(function(){
     $(this).hide();
   });
 
+     $('.slideout-menu-toggle').on('click', function(event){
+      event.preventDefault();
+      // create menu variables
+      var slideoutMenu = $('.slideout-menu');
+      var slideoutMenuWidth = $('.slideout-menu').width();
+      
+      // toggle open class
+      slideoutMenu.toggleClass("open");
+      
+      // slide menu
+      if (slideoutMenu.hasClass("open")) {
+        slideoutMenu.animate({
+          right: "0px"
+        }); 
+      } else {
+        slideoutMenu.animate({
+          right: -slideoutMenuWidth
+        }, 250);  
+      }
+    });
+
 });
